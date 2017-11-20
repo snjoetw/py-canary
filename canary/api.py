afac72a1a5ac26221735059731e7d943e391f79a
Joe Lu <jlu9@apple.com>
2017-11-20 00:07:41 -0800

    Moved to use Canary mobile API

--- canary/api.py
+++ canary/api.py
@@ -1,30 +1,38 @@
 import logging
+from datetime import datetime, timedelta
 from enum import Enum
 
 import requests
 
-COOKIE_XSRF_TOKEN = "XSRF-TOKEN"
-COOKIE_SSESYRANAC = "ssesyranac"
-
-COOKIE_VALUE_SSESYRANAC = "token={}"
-
-HEADER_XSRF_TOKEN = "X-XSRF-TOKEN"
-HEADER_SSESYRANAC = "ssesyranac"
 HEADER_AUTHORIZATION = "Authorization"
+HEADER_USER_AGENT = "User-Agent"
 
 HEADER_VALUE_AUTHORIZATION = "Bearer {}"
+HEADER_VALUE_USER_AGENT = "Canary/2.10.0 (iPhone; iOS 11.2; Scale/3.00)"
 
-URL_LOGIN_PAGE = "https://my.canary.is/login"
-URL_LOGIN_API = "https://my.canary.is/api/auth/login"
-URL_ME_API = "https://my.canary.is/api/customers/me?email={}"
-URL_LOCATIONS_API = "https://my.canary.is/api/locations"
-URL_READINGS_API = "https://my.canary.is/api/readings?deviceId={}&type={}"
-URL_ENTRIES_API = "https://my.canary.is/api/entries/{}?entry_type={}&limit={}&offset=0"
+URL_LOGIN_API = "https://api.canaryis.com/o/access_token/"
+URL_LOCATIONS_API = "https://api.canaryis.com/v1/locations/"
+URL_MODES_API = "https://api.canaryis.com/v1/modes/"
+URL_ENTRIES_API = "https://api.canaryis.com/v1/entries/"
+URL_READINGS_API = "https://api.canaryis.com/v1/readings/"
 
 ATTR_USERNAME = "username"
 ATTR_PASSWORD = "password"
+ATTR_CLIENT_ID = "client_id"
+ATTR_CLIENT_SECRET = "client_secret"
+ATTR_GRANT_TYPE = "grant_type"
+ATTR_SCOPE = "scope"
+
+ATTR_VALUE_CLIENT_ID = "a183323eab0544d83808"
+ATTR_VALUE_CLIENT_SECRET = "ba883a083b2d45fa7c6a6567ca7a01e473c3a269"
+ATTR_VALUE_GRANT_TYPE = "password"
+ATTR_VALUE_SCOPE = "write"
+
+DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
 
 _LOGGER = logging.getLogger(__name__)
+
+RECORDING_MODE_NAMES = ["armed", "disarmed"]
 
 
 class Api:
@@ -33,80 +41,82 @@ class Api:
         self._password = password
         self._timeout = timeout
         self._token = None
-        self._xsrf_token = None
 
     def login(self):
-        r = requests.get(URL_LOGIN_PAGE)
-
-        xsrf_token = r.cookies[COOKIE_XSRF_TOKEN]
-        ssesyranac = r.cookies[COOKIE_SSESYRANAC]
-
         r = requests.post(URL_LOGIN_API, {
             ATTR_USERNAME: self._username,
-            ATTR_PASSWORD: self._password
+            ATTR_PASSWORD: self._password,
+            ATTR_CLIENT_ID: ATTR_VALUE_CLIENT_ID,
+            ATTR_CLIENT_SECRET: ATTR_VALUE_CLIENT_SECRET,
+            ATTR_GRANT_TYPE: ATTR_VALUE_GRANT_TYPE,
+            ATTR_SCOPE: ATTR_VALUE_SCOPE,
         }, headers={
-            HEADER_XSRF_TOKEN: xsrf_token
-        }, cookies={
-            COOKIE_XSRF_TOKEN: xsrf_token,
-            COOKIE_SSESYRANAC: ssesyranac
+            HEADER_USER_AGENT: HEADER_VALUE_USER_AGENT
         })
 
         self._token = r.json()["access_token"]
-        self._xsrf_token = xsrf_token
+        self._modes_by_name = {mode.name: mode for mode in self.get_modes()}
 
-    def get_me(self):
-        r = requests.get(URL_ME_API.format(self._username),
-                         headers=self._api_headers(),
-                         cookies=self._api_cookies())
-        r.raise_for_status()
-
-        _LOGGER.debug("Received get_me API response: {}".format(r.json()))
-
-        return Customer(r.json())
+    def get_modes(self):
+        json = self._call_api("get", URL_MODES_API).json()["objects"]
+        return [Mode(data) for data in json]
 
     def get_locations(self):
-        r = requests.get(URL_LOCATIONS_API,
-                         headers=self._api_headers(),
-                         cookies=self._api_cookies())
+        json = self._call_api("get", URL_LOCATIONS_API).json()["objects"]
+        return [Location(data, self._modes_by_name) for data in json]
+
+    def get_readings(self, device_id):
+        end = datetime.utcnow()
+        start = end - timedelta(hours=2)
+        created_range = "{},{}".format(start.strftime(DATETIME_FORMAT),
+                                       end.strftime(DATETIME_FORMAT))
+        json = self._call_api("get", URL_READINGS_API, {
+            "created__range": created_range,
+            "device": device_id,
+            "resolution": "10m",
+            "limit": 0
+        }).json()["objects"]
+        return [Reading(data) for data in json]
+
+    def get_latest_readings(self, device_id):
+        readings = self.get_readings(device_id)
+        readings_by_type = {}
+
+        for reading in readings:
+            if reading.sensor_type not in readings_by_type:
+                readings_by_type[reading.sensor_type] = reading
+
+        return readings_by_type.values()
+
+    def get_entries(self, location_id, entry_type="motion", limit=1,
+                    last_modified=None):
+        if last_modified is None:
+            last_modified = datetime.utcnow() - timedelta(days=3)
+
+        json = self._call_api("get", URL_ENTRIES_API, {
+            "last_modified__gt": last_modified.strftime(DATETIME_FORMAT),
+            "include_deleted": "True",
+            "offset": "0",
+            "location": location_id,
+            "limit": limit,
+            "entry_type": entry_type,
+        }).json()["objects"]
+        return [Entry(data) for data in json]
+
+    def _call_api(self, method, url, params=None, **kwargs):
+        _LOGGER.debug("About to call {} with {}".format(url, params))
+
+        r = requests.request(method, url, params=params, timeout=self._timeout,
+                             headers=self._api_headers(), **kwargs)
         r.raise_for_status()
 
-        _LOGGER.debug(
-            "Received get_locations API response: {}".format(r.json()))
+        _LOGGER.debug("Received API response: {}".format(r.content))
 
-        return [Location(data) for data in r.json()]
-
-    def get_readings(self, device):
-        r = requests.get(
-            URL_READINGS_API.format(device.device_id, device.device_type),
-            headers=self._api_headers(),
-            cookies=self._api_cookies())
-        r.raise_for_status()
-
-        _LOGGER.debug(
-            "Received get_readings API response: {}".format(r.json()))
-
-        return [Reading(data) for data in r.json()]
-
-    def get_entries(self, location_id, entry_type="motion", limit=6):
-        r = requests.get(
-            URL_ENTRIES_API.format(location_id, entry_type, limit),
-            headers=self._api_headers(),
-            cookies=self._api_cookies())
-        r.raise_for_status()
-
-        _LOGGER.debug("Received get_entries API response: {}".format(r.json()))
-
-        return [Entry(data) for data in r.json()]
-
-    def _api_cookies(self):
-        return {
-            COOKIE_XSRF_TOKEN: self._xsrf_token,
-            COOKIE_SSESYRANAC: COOKIE_VALUE_SSESYRANAC.format(self._token)
-        }
+        return r
 
     def _api_headers(self):
         return {
-            HEADER_XSRF_TOKEN: self._xsrf_token,
+            HEADER_USER_AGENT: HEADER_VALUE_USER_AGENT,
             HEADER_AUTHORIZATION: HEADER_VALUE_AUTHORIZATION.format(
                 self._token)
         }
@@ -137,17 +147,25 @@ class Customer:
 
 
 class Location:
-    def __init__(self, data):
+    def __init__(self, data, modes_by_name):
         self._id = data["id"]
         self._name = data["name"]
         self._resource_uri = data["resource_uri"]
-        self._location_mode = None if data["mode"] is None \
-            else LocationMode(data["mode"])
         self._is_private = data["is_private"]
         self._devices = []
+        self._customers = []
+
+        mode_name = data.get("mode", {}).get("name", None);
+        self._mode = modes_by_name.get(mode_name, None)
+
+        current_mode_name = data.get("current_mode", {}).get("name", None);
+        self._current_mode = modes_by_name.get(current_mode_name, None)
 
         for device_data in data["devices"]:
             self._devices.append(Device(device_data))
+
+        for customer_data in data["customers"]:
+            self._customers.append(Customer(customer_data))
 
     @property
     def location_id(self):
@@ -158,8 +176,12 @@ class Location:
         return self._name
 
     @property
-    def location_mode(self):
-        return self._location_mode
+    def mode(self):
+        return self._mode
+
+    @property
+    def current_mode(self):
+        return self._current_mode
 
     @property
     def resource_uri(self):
@@ -170,14 +192,19 @@ class Location:
         return self._devices
 
     @property
+    def customers(self):
+        return self._customers
+
+    @property
     def is_private(self):
         return self._is_private
 
+    @property
+    def is_recording(self):
+        if self.current_mode is None:
+            return False
 
-class LocationMode(Enum):
-    AWAY = "away"
-    HOME = "home"
-    NIGHT = "night"
+        return self.current_mode.name in RECORDING_MODE_NAMES
 
 
 class Device:
@@ -185,8 +212,7 @@ class Device:
         self._id = data["id"]
         self._uuid = data["uuid"]
         self._name = data["name"]
-        self._device_mode = None if data["device_mode"] is None \
-            else DeviceMode(data["device_mode"])
+        self._device_mode = None
         self._is_online = data["online"]
         self._device_type = data["device_type"]
 
@@ -215,25 +241,14 @@ class Device:
         return self._device_type
 
 
-class DeviceMode(Enum):
-    DISARMED = "disarmed"
-    ARMED = "armed"
-    PRIVACY = "privacy"
-
-
 class Reading:
     def __init__(self, data):
-        self._sensor_type = SensorType(data["sensor_type"])
-        self._status = data["status"]
+        self._sensor_type = SensorType(data["sensor_type"]["name"])
         self._value = data["value"]
 
     @property
     def sensor_type(self):
         return self._sensor_type
-
-    @property
-    def status(self):
-        return self._status
 
     @property
     def value(self):
@@ -290,13 +305,30 @@ class Entry:
 
 class Thumbnail:
     def __init__(self, data):
-        self._device_uuid = data["device_uuid"]
         self._image_url = data["image_url"]
-
-    @property
-    def device_uuid(self):
-        return self._device_uuid
 
     @property
     def image_url(self):
         return self._image_url
+
+
+class Mode:
+    def __init__(self, data):
+        self._id = data["id"]
+        self._name = data["name"]
+        self._resource_uri = data["resource_uri"]
+
+    def __repr__(self):
+        return "Mode(id={}, name={})".format(self.mode_id, self.name)
+
+    @property
+    def mode_id(self):
+        return self._id
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def resource_uri(self):
+        return self._resource_uri
