7727496c3ec9e3f32becb5b239513b1329846096
Joe Lu <jlu9@apple.com>
2017-12-28 23:32:44 -0800

    Bumped version to 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='py-canary',
-    version='0.2.4',
+    version='0.3.0',
     packages=['canary'],
     url='https://github.com/snjoetw/py-canary',
     license='MIT',
